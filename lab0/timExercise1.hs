f99f54687a932dd77acc895904f4aea598dd510e
timales <125740870+timales@users.noreply.github.com>
2023-09-06 16:35:08 +0200

    small changes2

--- lab0/timExercise1.hs
+++ lab0/timExercise1.hs
@@ -1,21 +1,21 @@
-proof1 :: [Int]
-proof1 = map (^2) [1..]
+sequence1 :: [Int]
+sequence1 = map (^2) [1..]
 
 equation1 :: Int -> Int
 equation1 n = (n*(n+1)*(2*n+1)) `div` 6
 
 answer1 :: Int -> Int
-answer1 n = sum (take n proof1)
+answer1 n = sum (take n sequence1)
 
 -- next equation
 
-proof2 :: [Int]
-proof2 = map (^3) [1..]
+sequence2 :: [Int]
+sequence2 = map (^3) [1..]
 
 equation2 :: Int -> Int
 equation2 n = ((n*(n+1)) `div` 2) ^ 2
 
 answer2 :: Int -> Int
-answer2 n = sum (take n proof2)
+answer2 n = sum (take n sequence2)
 
 --30 minutes
