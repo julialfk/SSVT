e95396948a9af0d585906fbfdbea9c397c599b5e
timales <125740870+timales@users.noreply.github.com>
2023-09-07 16:13:55 +0200

    Update timExercise5.hs

--- lab0/Tim/timExercise5.hs
+++ lab0/Tim/timExercise5.hs
@@ -8,23 +8,21 @@ rot13 (x:xs)
     | isAsciiUpper x = upperRotation x : rot13 xs
     | otherwise      = x : rot13 xs
 
-
 lowerRotation :: Char -> Char
 lowerRotation c = chr (((ord c - ord 'a' + 13) `mod` 26) + ord 'a')
 
 upperRotation :: Char -> Char
 upperRotation c = chr (((ord c - ord 'A' + 13) `mod` 26) + ord 'A')
 
--- isAlphabetic :: [Char] -> Bool
--- isAlphabetic = all isAlpha
-
--- prop_rot13_twice :: [Char] -> Property
--- prop_rot13_twice s = not (null s) ==> isAlphabetic s ==> rot13 (rot13 s) == s
-
--- prop_rot13_once :: [Char] -> Property
--- prop_rot13_once s = not (null s) ==> isAlphabetic s ==> rot13 s /= s
+propROT13 :: [Char] -> Bool
+propROT13 s = rot13 (rot13 s) == s
 
 main :: IO ()
 main = do
     print (rot13 "abc def")
     print (rot13 "nop qrs")
+    quickCheck propROT13
+
+-- First print shows that rot13, changes order of charSize
+-- QuickCheck used to see if rotating twice also gives back
+-- same string.
