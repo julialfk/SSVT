2aadc5f0402bd1d1c00e0ed42f3cbb28170fcbad
timales <125740870+timales@users.noreply.github.com>
2023-09-13 16:38:52 +0200

    small changes

--- lab1/Tim/Exercise2.hs
+++ lab1/Tim/Exercise2.hs
@@ -1,6 +1,13 @@
+import Data.List
+import System.Random
+import Test.QuickCheck
+-- import Lecture1
+
 subsequences :: [a] -> [[a]]
 
 subsequences [] = [[]]
 subsequences (x:xs) = subsequences xs ++ map (x:) (subsequences xs)
 
-
+-- Added a conditional to only allow natural numbers.
+prop_length :: [a] -> Property
+prop_length l = length (subsequences l) == ( 2^ length(l))
--- lab1/Tim/Exercise4.hs
+++ lab1/Tim/Exercise4.hs
@@ -1,5 +1,10 @@
+import Data.List
+import System.Random
+import Test.QuickCheck
+-- import Lecture1
+
 perms :: [a] ->[[a]]
-> perms [] = [[]]
-> perms (x:xs) = concat (map (insrt x) (perms xs)) where
-> insrt x [] = [[x]]
-> insrt x (y:ys) = (x:y:ys) : map (y:) (insrt x ys
+perms [] = [[]]
+perms (x:xs) = concat (map (insrt x) (perms xs)) where
+insrt x [] = [[x]]
+insrt x (y:ys) = (x:y:ys) : map (y:) (insrt x ys)
