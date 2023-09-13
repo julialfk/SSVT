0cbe17a1b8a6ecb96ca99999c1e34010ee5c60f6
timales <125740870+timales@users.noreply.github.com>
2023-09-13 16:44:48 +0200

    small changes

--- lab1/Tim/Exercise2.hs
+++ lab1/Tim/Exercise2.hs
@@ -10,3 +10,10 @@ subsequences (x:xs) = subsequences xs ++ map (x:) (subsequences xs)
 
 prop_length :: [a] -> Property
 prop_length l = length (subsequences l) == ( 2^ length(l))
+
+alphaChar :: Gen Char
+alphaChar = elements ([0..9])
+
+-- Generates a list of lower and upper case characters
+alphaString :: Gen String
+alphaString = listOf alphaChar
