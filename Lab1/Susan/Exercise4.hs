327b9e04479fee50d66072d0d3d6208be0bcff5b
Seedot1234 <supersuus14@gmail.com>
2023-09-21 14:25:03 +0200

    code Susan

---
+++ Lab1/Susan/Exercise4.hs
@@ -0,0 +1,27 @@
+import System.Random
+import Test.QuickCheck
+import Data.List
+
+main :: IO ()
+main = do
+    quickCheck (forAll genDerangements prop_Reverse)
+
+isDerangement :: Eq a => [a] -> [a] -> Bool
+isDerangement [] [] = True
+isDerangement [] _ = False
+isDerangement _ [] = False
+isDerangement (x:xs) (y:ys) | x == y = False
+                            | otherwise = isDerangement xs ys
+
+deran:: Int -> [[Int]] 
+deran n =  [permutation | permutation <- permutations [0..n-1], isDerangement permutation [0..n-1]]
+
+
+prop_Reverse :: [a] -> Property
+prop_Reverse xs = property (isDerangement xs (reverse xs))
+
+genDerangements :: Gen [Int]
+genDerangements = do
+  n <- arbitrary `suchThat` (\n -> n `mod` 2 == 0 && n <= 100) --choose (2, 25)
+  originalList <- shuffle [1..n]
+  return originalList
