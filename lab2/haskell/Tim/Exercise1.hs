9814153181824c853b2f7cc9203f3f187f5c5c13
timales <125740870+timales@users.noreply.github.com>
2023-09-28 11:18:29 +0200

    new push

--- lab2/haskell/Tim/Exercise1.hs
+++ lab2/haskell/Tim/Exercise1.hs
@@ -6,28 +6,22 @@ import Test.QuickCheck
 
 --type IOLTS = ([State], [Label], [Label], [LabeledTransition], State)
 
-
---validateLTS :: IOLTS -> Bool
---validateLTS (Q, L, T, q0) = True
---validateLTS (Q, Li, Lu, T, q0) = True
-
-
---myFunction :: [State] -> [Label] -> [Label] -> [LabeledTransition] -> State -> Bool
---myFunction q l t transitions q0 =
-
---n arbitrary
---[0..n]
-
--- gen single letters
-
--- pick random number left from 0 to n, pick random letter, pick random number right
-
--- pick random start state
+-- validateLTS :: IOLTS -> Bool
+-- validateLTS (q, li, lu, t, q0) = False if q is empty and not integer
+-- validateLTS (q, li, lu, t, q0) = False if L is not countable
+-- validateLTS (q, li, lu, t, q0) = False if q0 is not in q
+-- validateLTS (q, li, lu, t, q0) = True
 
 
--- tretmanS4 = ([0,1,2,3],["a"],["x"],[(0,"a",1),(1,"x",2),(1,"tau",3)],0)
+validateLTS :: IOLTS -> Bool
+validateLTS (q, li, lu, t, q0)
+    | null q = False
+    | null li = False
+    | null lu = False
+    | q0 `notElem` q = False
+    | otherwise = True
 
 main :: IO ()
 main = do
     print tretmanS4
-    --print validateLTS tretmanK3
+    print (validateLTS tretmanK3)
