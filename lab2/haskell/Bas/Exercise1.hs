79d37124c46cc9811b9982e66bc41fd8c06df1e4
Bas Kooter <bjkooter@gmail.com>
2023-09-28 13:29:43 +0200

    ex1

--- lab2/haskell/Bas/Exercise1.hs
+++ lab2/haskell/Bas/Exercise1.hs
@@ -4,17 +4,6 @@ import Data.List
 import LTS
 import Test.QuickCheck
 
--- 
-
---type IOLTS = ([State], [Label], [Label], [LabeledTransition], State)
-
--- validateLTS :: IOLTS -> Bool
--- validateLTS (q, li, lu, t, q0) = False if q is empty and not integer
--- validateLTS (q, li, lu, t, q0) = False if L is not countable
--- validateLTS (q, li, lu, t, q0) = False if q0 is not in q
--- validateLTS (q, li, lu, t, q0) = True
-
--- Check for duplicates
 validateLTS :: IOLTS -> Bool
 validateLTS (q, li, lu, t, q0)
     | null q = False
