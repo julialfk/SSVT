1342863b8c358167febb36408162e7e859a4c98f
timales <125740870+timales@users.noreply.github.com>
2023-09-28 12:44:24 +0200

    push

--- lab2/haskell/Tim/Exercise2.hs
+++ lab2/haskell/Tim/Exercise2.hs
@@ -7,27 +7,36 @@ genLabel = do
     char <- elements ['a'..'z']
     return [char]
 
-generateLabels :: Gen ([Label], [Label])
-generateLabels = do
+genLabels :: Gen ([Label], [Label])
+genLabels = do
     n <- choose (1, 5)
     li <- vectorOf n genLabel
     m <- choose (1, 5)
     lu <- vectorOf m genLabel
     if not (null (lu \\ li))
         then return (li, lu)
-        else generateLabels
+        else genLabels
+
+genTransition :: [State] -> [Label] -> [Label] -> State -> Int -> Gen [(State, Label, State)]
+genTransition _ _ _ _ 0 = return []
+genTransition q li lu s n = do
+    q1 <- elements q
+    reaction <- if even n then elements li else elements lu
+    rest <- genTransition q li lu q1 (n - 1)
+    return ((s, reaction, q1) : rest)
 
 genIOLTS :: Gen IOLTS
 genIOLTS = do
     n <- choose (0, 10)
     let q = [0..n]
-    (li, lu) <- generateLabels
-    --transitions here
+    (li, lu) <- genLabels
+    m <- choose (1, 20)
     q0 <- elements q
-    return (q, li, lu, [], q0)
+    t <- genTransition q li lu q0 m
+    return (q, li, lu, t, q0)
 
 main :: IO ()
 main = do
     putStrLn "Sample IOLTS:"
-    samples <- generate (vectorOf 5 genIOLTS)  -- Generate 5 samples
+    samples <- generate (vectorOf 5 genIOLTS)
     mapM_ print samples
