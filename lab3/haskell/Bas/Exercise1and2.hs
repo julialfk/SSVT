0d1a80a495f9ef50f28c714b43a5ffaa3a156ca7
timales <125740870+timales@users.noreply.github.com>
2023-10-05 15:07:03 +0200

    comments

--- lab3/haskell/Bas/Exercise1and2.hs
+++ lab3/haskell/Bas/Exercise1and2.hs
@@ -23,31 +23,27 @@ changeElement xs = do
 emptyList :: [a] -> Gen [b]
 emptyList _ = return []
 
+-- Counts the surviving mutations that will not fail the properties
 countSurvivors :: Integer -> [([Integer] -> Integer -> Bool)] -> (Integer -> [Integer]) -> IO Int
 countSurvivors n xs f = do
-    survivors <- generate $ vectorOf 4000 (getSurvived xs f)
+    survivors <- generate $ vectorOf n (getSurvived xs f)
     return (sum survivors)
 
+-- Gets the surviving mutations
 getSurvived :: [([Integer] -> Integer -> Bool)] -> (Integer -> [Integer]) -> Gen Int
 getSurvived xs f = do
-    result <- mutateFunction xs f
-    points <- isKilled result
-    return points
+  result <- mutateFunction xs f
+  isKilled result
 
+-- Returns if an mutation is killed, or survived
 isKilled :: Bool -> Gen Int
 isKilled action = do
     return $ if action then 1 else 0
 
+--
 mutateFunction :: [([Integer] -> Integer -> Bool)] -> (Integer -> [Integer]) -> Gen Bool
 mutateFunction xs f = do
     input <- choose(0,100)
     mutation <- elements [addElements, removeElements, anyList]
     result <- mutate' mutation xs f input
-    if (length result) == 0 then return False else return (all (== True) result)
-
-main :: IO ()
-main = do
-  -- Generate and print 5 samples
-    generatedSample <- generate (charList [1,2,3,4])
-
-    print generatedSample
+    if length result == 0 then return False else return (all (== True) result)
