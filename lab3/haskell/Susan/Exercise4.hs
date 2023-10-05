eb504273f641962706a8fd6fd83d0d56197b1a7c
Seedot1234 <supersuus14@gmail.com>
2023-10-05 17:07:49 +0200

    Ex4 done. Ex6 work in progress

--- lab3/haskell/Susan/Exercise4.hs
+++ lab3/haskell/Susan/Exercise4.hs
@@ -11,16 +11,21 @@ main = do
     result <- propertySetStrength 4000 multiplicationTableProps multiplicationTable
     print result
 
--- Time spent: 15 minutes
+-- Time spent: 30 minutes
 
--- Implement a function that calculates the strength of a given set of properties,
--- which is the percentage of mutants they kill.
+-- "Implement a function that calculates the strength of a given set of properties,
+-- which is the percentage of mutants they kill"
 
--- so: (total number of mutants - survivors) / total number of mutants
+-- This means the following: (total number of mutants - survivors) / total number of mutants
 
--- is the total number of mutants arbitrary? Can I make this up myself?
--- I suspect that there is a minimum amount for the ratio to actually say something
--- We could set it to, say, 4000 always. Or we could pick a random number between, say, 3000 and 5000
+-- At first we randomly generated the total number of mutants inside of this function. We changed this in order to be able
+-- to integrate this function in other functions introduced later in the assignment.
+-- There was a thought process here about what number of mutants is a good number. FitSpec has a default number of mutants of 4000.
+-- This felt reasonable. The number shoulnd't be too low, because then it might be too hard to find a surviving mutant.
+-- The number also shouldn't be too high, because then the computation takes too long.
+-- In the old implementation, we took a random number in the range (3500, 4500). This was still somewhat arbitrary, but a
+-- reasonable solution. We will stick to the 4000 mutants in the future, because this corresponds to the FitSpec research paper and
+-- we trust their judgement.
 
 propertySetStrength :: Int -> [([Integer] -> Integer -> Bool)] -> (Integer -> [Integer]) -> IO Float
 propertySetStrength totalNumberOfMutants listOfProperties functionUnderTest = do
--- lab3/haskell/Susan/Exercise6.hs
+++ lab3/haskell/Susan/Exercise6.hs
@@ -4,21 +4,54 @@ import Data.List
 import Test.QuickCheck
 import Mutation
 import Text.Printf
-import Exercise1
-import Exercise4
+import Exercise2
+import qualified Exercise4 as Ex4
+import MultiplicationTable
 
--- Time spent so far: 30 minutes
+-- Time spent so far: 1 hour
 
 -- Create a function that we pass the function under test, a set of properties, and a number
 -- indicating the number of mutants, that visualizes the results of the functions from the
 -- previous exercises.
 
-resultVisualiser :: Integer -> [([Integer] -> Integer -> Property)] -> (Integer -> [Integer]) -> IO ()
+-- We took the output of FitSpec (see below) as a framework, and tried to stay as close to it as possible.
+-- Text.Printf is amazing.
+
+main :: IO ()
+main = do
+    resultVisualiser 4000 multiplicationTableProps multiplicationTable
+
+
+printListItems :: Int -> [a] -> IO ()
+printListItems _ [] =
+    printf"\n"
+printListItems 1 (x:xs) = do
+    printf "1"
+    printListItems (2) xs
+printListItems index [x] = do
+    printf " and %d" index
+    printListItems (index + 1) []
+printListItems index (x:xs) = do
+    printf", %d" index
+    printListItems (index + 1) xs
+
+resultVisualiser :: Int -> [([Integer] -> Integer -> Bool)] -> (Integer -> [Integer]) -> IO ()
 resultVisualiser totalNumberOfMutants listOfProperties functionUnderTest = do
-    let survivors = countSurvivors totalNumberOfMutants listOfProperties functionUnderTest
-    let strength = propertySetStrength totalNumberOfMutants listOfProperties functionUnderTest * 100
-    printf "Tested the given function with %d mutants.\n" totalNumberOfMutants
-    printf "%d survivors (%.3f%% killed).\n" survivors percentageKilled
+    survivors <- countSurvivors totalNumberOfMutants listOfProperties functionUnderTest
+    strength <- Ex4.propertySetStrength totalNumberOfMutants listOfProperties functionUnderTest
+    printf"Apparent "
+    if survivors == 0 then
+            printf "complete (..minimal..) "
+        else
+            printf "incomplete (..minimal..) "
+    printf "specification based on\n"
+    printf "%d test cases for each of properties " totalNumberOfMutants
+    printListItems 1 listOfProperties
+    printf "for each of %d mutant variations.\n\n" totalNumberOfMutants
+    printf "%d survivors (%.2f%% killed).\n\n" survivors (strength * 100) 
+    printf "apparent minimal property subsets: \n"
+    printf "conjectures: \n"
+
 
 
 -- #############################################################
