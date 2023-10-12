997728e2bc91ce77b98f6d7d1e164b839d2b22b5
Bas Kooter <bjkooter@gmail.com>
2023-10-12 15:16:45 +0200

    fixed3

--- lab4/Bas/Exercise3.hs
+++ lab4/Bas/Exercise3.hs
@@ -1,20 +1,10 @@
 import Data.List
+import SetOrd
 
 type Rel a = [(a,a)]
 
 symClos :: Ord a => Rel a -> Rel a
 symClos [] = []
-symClos ((x,y):xs) = do
-    if elem (y,x) xs 
-    then 
-        do 
-            let newList = delete (y,x) xs
-            if x < y 
-            then [(x,y), (y,x)] ++ symClos newList
-            else [(y,x), (x,y)] ++ symClos newList
-    else 
-        if x < y 
-        then [(x,y), (y,x)] ++ symClos xs
-        else [(y,x), (x,y)] ++ symClos xs
+symClos ((x,y):xs) = sort $ nub $ (x,y):(y,x):symClos xs
 
 -- Time spent 10 minutes
