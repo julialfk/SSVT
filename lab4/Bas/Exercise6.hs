d88d4ef40c681d10aaf191597424c412849dfa9a
timales <125740870+timales@users.noreply.github.com>
2023-10-12 14:51:10 +0200

    push

--- lab4/Bas/Exercise6.hs
+++ lab4/Bas/Exercise6.hs
@@ -10,7 +10,15 @@ type Rel a = [(a,a)]
 genRelation :: Gen [(Int, Int)]
 genRelation = do
   n <- choose (0, 10)
-  sequence [genPair | _ <- [1..n]]
+  l <- vectorOf n genPair
+  return l
+
+genSymClosureRelation :: Gen [(Int, Int)]
+genSymClosureRelation = do
+  n <- choose (0, 5)
+  l1 <- vectorOf n genPair
+  let l2 = [(y, x) | (x, y) <- l1]
+  return $ nub $ l1 ++ l2
 
 genPair :: Gen (Int, Int)
 genPair = do
@@ -18,11 +26,11 @@ genPair = do
   y <- choose (1, 9)
   return (x, y)
 
-propSymExists :: Rel a -> Bool
-propSymExists [] = True
-propSymExists ((x,y):xs) = do
-    if elem (y,x) 
-        then propSymExists xs
-        else False
+-- propSymExists :: Rel a -> Bool
+-- propSymExists [] = True
+-- propSymExists ((x,y):xs) = do
+--     if elem (y,x)
+--         then propSymExists xs
+--         else False
 
 
