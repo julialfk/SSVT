df02b78414d1a3467336ec9bc3495b4e9e74394c
Seedot1234 <supersuus14@gmail.com>
2023-10-18 15:15:26 +0200

    Ex7 done + comments

--- lab4/Susan/Exercise7.hs
+++ lab4/Susan/Exercise7.hs
@@ -2,38 +2,37 @@ module Exercise7 where
 
 -- Time Spent: 15 minutes.
 
--- DIT IS WAT CHATGPT ER OVER TE ZEGGEN HAD. WSS NIET HEEL HANDIG OM DIT 1-OP-1 IN TE LEVEREN,
--- MAAR GEEFT WEL EEN GOED BEELD OVER WAT HET ANTWOORD ONGEVEER MOET ZIJN
--- MISSCHIEN DIT ANTWOORD COMBINEREN MET WAT JULIA AL ALS TEGENVOORBEELD HAD
+-- Is there a difference between the symmetric closure of the transitive closure of a relation R
+-- and the transitive closure of the symmetric closure of R ?
 
--- The symmetric closure and the transitive closure operations do not commute, which means that there is a difference between the symmetric closure of the transitive closure of a relation R and the transitive closure of the symmetric closure of R. Let me explain the difference and provide an example to illustrate it.
+-- Yes, there is a difference. These two sets are not the same. See below for an explanation:
 
 -- Symmetric Closure of the Transitive Closure:
+-- First you take the transitive closure of R and then the symmetric closure of the result.
+-- This ensures that if (a, b) is in the relation, then (b, a) will also be in there.
+-- This doesn't necessarily mean that the relation is transitive.
 
--- If you first take the transitive closure of a relation R and then take the symmetric closure of the result, you are ensuring that if (a, b) is in the relation, then (b, a) will also be included in the relation. This operation doesn't necessarily guarantee that the relation is transitive.
 -- Transitive Closure of the Symmetric Closure:
+-- First you take the symmetric closure of R and then take the transitive closure of the result.
+-- This ensures that if (a, b) is in the relation, and (b, a) is also in the relation, and then
+-- we add the transitive closure of these pairs.
+-- This doesn't necessarily mean that the relation is symmetric.
 
--- If you first take the symmetric closure of a relation R and then take the transitive closure of the result, you are ensuring that if (a, b) is in the relation, and (b, a) is also in the relation, then you add the transitive closure of these pairs. This operation doesn't necessarily guarantee that the relation is symmetric.
--- Here's an example to illustrate the difference:
-
--- Let R = {(1, 2), (2, 3)}. This is a non-transitive and non-symmetric relation.
+-- See the following counter example as a proof:
+-- R = [(1,2),(2,3),(3,4)]
 
 -- Transitive Closure of R:
-
--- The transitive closure of R would include (1, 3) because we can reach 3 from 1 through 2. However, it doesn't include (2, 1) or (3, 2) as those pairs are not originally in R.
--- Transitive Closure of R = {(1, 2), (2, 3), (1, 3)}
+    -- [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)]. We derived this from our function written in Exercise 5.
 
 -- Symmetric Closure of R:
+    -- [(1,2),(2,1),(2,3),(3,2),(3,4),(4,3)]. We derived this from our function written in Exercise 3.
 
--- The symmetric closure of R adds the reverse pairs for each element in R: (2, 1) and (3, 2).
--- Symmetric Closure of R = {(1, 2), (2, 3), (1, 3), (2, 1), (3, 2)}
+-- Symmetric Closure of the Transitive Closure:
+    -- [(1,2),(1,3),(1,4),(2,1),(2,3),(2,4),(3,1),(3,2),(3,4),(4,1),(4,2),(4,3)]. We derived this from our function written in Exercise 3.
 
--- Now, let's take the transitive closure of the symmetric closure from step 2:
+-- Transitive Closure of the Symmetric Closure:
+    -- [(1,2),(1,1),(2,1),(2,3),(2,2),(3,2),(3,4),(3,3),(4,3)]. We derived this from our function written in Exercise 5.
 
--- For instance, (1, 2) and (2, 1) are in the symmetric closure, and their transitive closure includes (1, 1), (2, 2), and (1, 2), (2, 1).
--- Transitive Closure of Symmetric Closure of R = {(1, 2), (2, 3), (1, 3), (2, 1), (3, 2), (1, 1), (2, 2), (1, 2), (2, 1)}
+-- As you can see, these two sets are not equal and therefore there is a difference between the symmetric
+-- closure of the transitive closure of a relation R and the transitive closure of the symmetric closure of R.
 
--- Now, let's take the symmetric closure of the transitive closure from step 1:
-
--- We already obtained this result in step 1, which is {(1, 2), (2, 3), (1, 3)}.
--- As you can see, the results of these two operations are different. Therefore, the order of applying the operations matters, and the two are not the same.
