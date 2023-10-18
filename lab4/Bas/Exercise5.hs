cf7195360c5829c8c6bfdd66b65647196c071573
Bas Kooter <bjkooter@gmail.com>
2023-10-18 15:13:07 +0200

    ex6

--- lab4/Bas/Exercise5.hs
+++ lab4/Bas/Exercise5.hs
@@ -6,11 +6,17 @@ infixr 5 @@
 (@@) :: Eq a => Rel a -> Rel a -> Rel a
 r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
 
+-- Recursively loop thorugh all elements of the set, adding the element itself
+-- and then add all its transitive relations in the rest of the list next to the
+-- element using the closeTillEnd function.
 trClos :: Ord a => Rel a -> Rel a
 trClos [] = []
 trClos (x:[]) = [x]
-trClos ((x,y):xs) = [(x,y)] ++ closeTillEnd [(x,y)] xs ++ trClos xs
+trClos ((x,y):xs) = sort $ nub $ [(x,y)] ++ closeTillEnd [(x,y)] xs ++ trClos xs
 
+-- Loop through all elements in the list, using the @@ function check if there
+-- is a transitive relation from the given r set to the current element in the list.
+-- If so, add it to the list.
 closeTillEnd :: Ord a => Rel a -> Rel a -> Rel a
 closeTillEnd r [] = []
 closeTillEnd r ((x,y):xs) = do
